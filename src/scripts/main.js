39b7854935184591d80cb384cf357c8ef28957e9
mymymy1303 <thehuy160897@gmail.com>
2018-08-30 01:19:27 +0700

    Finish client slider section. Finish all project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,7 +13,7 @@ $(function () {
 	})
 
 
-	// ------------------------------------
+	// ---------------------------------
 	//---------FEEDBACK SLIDER----------
 	//----------------------------------
 	$('#feedback_slider').owlCarousel({
@@ -24,4 +24,41 @@ $(function () {
 		margin: 0
 	})
 
+	// -------------------------------------
+	// -----------CLIENT-SLIDER-------------
+	// -------------------------------------
+	$("#client_slider").owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		dots: false,
+		items: 1,
+		smartSpeed: 500,
+		responsive: {
+			576: {
+				items: 2
+			},
+			768: {
+				//Adding autoplay slide for medium and above devices
+				//Hover to pause autoplay
+				autoplay: true,
+				autoplayTimeout: 2000,
+				autoplayHoverPause: true,
+				items: 3,
+				margin: 0
+			},
+			992: {
+				autoplay: true,
+				autoplayTimeout: 2000,
+				autoplayHoverPause: true,
+				items: 4
+			},
+			1200: {
+				autoplay: true,
+				autoplayTimeout: 2000,
+				autoplayHoverPause: true,
+				items: 6
+			}
+		}
+	})
 });
